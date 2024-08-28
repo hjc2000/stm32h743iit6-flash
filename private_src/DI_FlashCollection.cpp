27abf79d6180478b70e775af69204b6ff9d8ff71
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-29 00:25:37 +0800

    Collection

--- private_src/DI_FlashCollection.cpp
+++ private_src/DI_FlashCollection.cpp
@@ -1,4 +1,4 @@
-#include <base/container/StdMapValuesEnumerator.h>
+#include <base/container/Collection.h>
 #include <base/Initializer.h>
 #include <bsp-interface/di/flash.h>
 #include <Flash.h>
@@ -10,51 +10,24 @@ base::Initializer _initializer{
         DI_FlashCollection();
     }};
 
-class Collection :
-    public base::IReadOnlyCollection<std::string, bsp::IFlash *>
+class Collection
 {
-private:
-    std::map<std::string, bsp::IFlash *> _map;
-
-    void Add(bsp::IFlash *flash)
-    {
-        _map[flash->Name()] = flash;
-    }
-
 public:
     Collection()
     {
         Add(&hal::Flash::Instance());
     }
 
-    virtual int Count() const override
-    {
-        return _map.size();
-    }
+    base::Collection<std::string, bsp::IFlash *> _collection;
 
-    virtual bsp::IFlash *Get(std::string key) const override
+    void Add(bsp::IFlash *flash)
     {
-        auto it = _map.find(key);
-        if (it == _map.end())
-        {
-            throw std::runtime_error{"找不到该 flash"};
-        }
-
-        return it->second;
-    }
-
-    std::shared_ptr<base::IEnumerator<bsp::IFlash *>> GetEnumerator() override
-    {
-        return std::shared_ptr<base::IEnumerator<bsp::IFlash *>>{
-            new base::StdMapValuesEnumerator<std::string, bsp::IFlash *>{
-                &_map,
-            },
-        };
+        _collection.Put(flash->Name(), flash);
     }
 };
 
-base::IReadOnlyCollection<std::string, bsp::IFlash *> &DI_FlashCollection()
+base::ICollection<std::string, bsp::IFlash *> const &DI_FlashCollection()
 {
     static Collection o;
-    return o;
+    return o._collection;
 }
