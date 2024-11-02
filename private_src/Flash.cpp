7d40d9e8f1f01f08d0330c3a407d84b916b1b12d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-02 22:41:04 +0800

    Flash

--- private_src/Flash.cpp
+++ private_src/Flash.cpp
@@ -76,6 +76,12 @@ bsp::Flash &bsp::Flash::Instance()
     return g.Instance();
 }
 
+std::string bsp::Flash::Name()
+{
+    // 片内 flash
+    return "internal-flash";
+}
+
 void bsp::Flash::Lock()
 {
     HAL_StatusTypeDef result = HAL_FLASH_Lock();
@@ -94,28 +100,95 @@ void bsp::Flash::Unlock()
     }
 }
 
-size_t bsp::Flash::GetBankBaseAddress(int32_t bank_index) const
+size_t bsp::Flash::SectorSize() const
 {
-    switch (bank_index)
+    return static_cast<size_t>(128) << 10;
+}
+
+int32_t bsp::Flash::SectorCount() const
+{
+    return 8;
+}
+
+size_t bsp::Flash::BaseAddress() const
+{
+    return 0x08100000;
+}
+
+int32_t bsp::Flash::MinProgrammingUnit() const
+{
+    return 32;
+}
+
+void bsp::Flash::Erase()
+{
+    FLASH_EraseInitTypeDef def;
+
+    // 擦除类型为 bank 擦除
+    def.TypeErase = FLASH_TYPEERASE_MASSERASE;
+
+    // 选择 bank
+    def.Banks = FLASH_BANK_2;
+
+    // 每次擦除 64 位，直到擦除整个 bank。
+    // 越高的电压擦除时并行的位数就越多。
+    def.VoltageRange = FLASH_VOLTAGE_RANGE_4;
+
+    /* 擦除到某个扇区的时候如果发生错误，会停止擦除，将扇区索引赋值给 error_sector_index，
+     * 然后返回。
+     */
+    HAL_StatusTypeDef result = HAL_FLASHEx_Erase_IT(&def);
+    if (result != HAL_StatusTypeDef::HAL_OK)
     {
-    case 0:
-        {
-            return 0x08000000;
-        }
-    case 1:
-        {
-            return 0x08100000;
-        }
-    default:
-        {
-            throw std::out_of_range{"bank_index 超出范围"};
-        }
+        throw std::runtime_error{"启动擦除流程失败"};
     }
+
+    _operation_completed->Acquire();
+    if (_operation_failed)
+    {
+        throw std::runtime_error{"擦除流程结束，出错了"};
+    }
+
+    SCB_CleanInvalidateDCache();
+}
+
+void bsp::Flash::EraseSector(int32_t sector_index)
+{
+    FLASH_EraseInitTypeDef def;
+
+    // 擦除类型为扇区擦除
+    def.TypeErase = FLASH_TYPEERASE_SECTORS;
+
+    // 选择 bank
+    def.Banks = FLASH_BANK_2;
+    def.Sector = SectorIndexToDefine(sector_index);
+    def.NbSectors = 1;
+
+    // 每次擦除 64 位，直到擦除整个 bank。
+    // 越高的电压擦除时并行的位数就越多。
+    def.VoltageRange = FLASH_VOLTAGE_RANGE_4;
+
+    /* 擦除到某个扇区的时候如果发生错误，会停止擦除，将扇区索引赋值给 error_sector_index，
+     * 然后返回。
+     */
+    HAL_StatusTypeDef result = HAL_FLASHEx_Erase_IT(&def);
+    if (result != HAL_StatusTypeDef::HAL_OK)
+    {
+        throw std::runtime_error{"启动擦除流程失败"};
+    }
+
+    _operation_completed->Acquire();
+    if (_operation_failed)
+    {
+        throw std::runtime_error{"擦除流程结束，出错了"};
+    }
+
+    SCB_CleanInvalidateDCache();
 }
 
 #pragma region 擦除
 
-void bsp::Flash::EraseBank(int32_t bank_index)
+void bsp::Flash::EraseBank_NoIT()
 {
     FLASH_EraseInitTypeDef def;
 
@@ -123,71 +196,7 @@ void bsp::Flash::EraseBank(int32_t bank_index)
     def.TypeErase = FLASH_TYPEERASE_MASSERASE;
 
     // 选择 bank
-    switch (bank_index)
-    {
-    case 0:
-        {
-            def.Banks = FLASH_BANK_1;
-            break;
-        }
-    case 1:
-        {
-            def.Banks = FLASH_BANK_2;
-            break;
-        }
-    default:
-        {
-            throw std::out_of_range{"bank_index 超出范围"};
-        }
-    }
-
-    // 每次擦除 64 位，直到擦除整个 bank。
-    // 越高的电压擦除时并行的位数就越多。
-    def.VoltageRange = FLASH_VOLTAGE_RANGE_4;
-
-    /* 擦除到某个扇区的时候如果发生错误，会停止擦除，将扇区索引赋值给 error_sector_index，
-     * 然后返回。
-     */
-    HAL_StatusTypeDef result = HAL_FLASHEx_Erase_IT(&def);
-    if (result != HAL_StatusTypeDef::HAL_OK)
-    {
-        throw std::runtime_error{"启动擦除流程失败"};
-    }
-
-    _operation_completed->Acquire();
-    if (_operation_failed)
-    {
-        throw std::runtime_error{"擦除流程结束，出错了"};
-    }
-
-    SCB_CleanInvalidateDCache();
-}
-
-void bsp::Flash::EraseBank_NoIT(int32_t bank_index)
-{
-    FLASH_EraseInitTypeDef def;
-
-    // 擦除类型为 bank 擦除
-    def.TypeErase = FLASH_TYPEERASE_MASSERASE;
-
-    // 选择 bank
-    switch (bank_index)
-    {
-    case 0:
-        {
-            def.Banks = FLASH_BANK_1;
-            break;
-        }
-    case 1:
-        {
-            def.Banks = FLASH_BANK_2;
-            break;
-        }
-    default:
-        {
-            throw std::out_of_range{"bank_index 超出范围"};
-        }
-    }
+    def.Banks = FLASH_BANK_2;
 
     // 每次擦除 64 位，直到擦除整个 bank。
     // 越高的电压擦除时并行的位数就越多。
@@ -206,7 +215,7 @@ void bsp::Flash::EraseBank_NoIT(int32_t bank_index)
     SCB_CleanInvalidateDCache();
 }
 
-void bsp::Flash::EraseSector(int32_t bank_index, int32_t sector_index)
+void bsp::Flash::EraseSector_NoIT(int32_t sector_index)
 {
     FLASH_EraseInitTypeDef def;
 
@@ -214,75 +223,7 @@ void bsp::Flash::EraseSector(int32_t bank_index, int32_t sector_index)
     def.TypeErase = FLASH_TYPEERASE_SECTORS;
 
     // 选择 bank
-    switch (bank_index)
-    {
-    case 0:
-        {
-            def.Banks = FLASH_BANK_1;
-            break;
-        }
-    case 1:
-        {
-            def.Banks = FLASH_BANK_2;
-            break;
-        }
-    default:
-        {
-            throw std::out_of_range{"bank_index 超出范围"};
-        }
-    }
-
-    def.Sector = SectorIndexToDefine(sector_index);
-    def.NbSectors = 1;
-
-    // 每次擦除 64 位，直到擦除整个 bank。
-    // 越高的电压擦除时并行的位数就越多。
-    def.VoltageRange = FLASH_VOLTAGE_RANGE_4;
-
-    /* 擦除到某个扇区的时候如果发生错误，会停止擦除，将扇区索引赋值给 error_sector_index，
-     * 然后返回。
-     */
-    HAL_StatusTypeDef result = HAL_FLASHEx_Erase_IT(&def);
-    if (result != HAL_StatusTypeDef::HAL_OK)
-    {
-        throw std::runtime_error{"启动擦除流程失败"};
-    }
-
-    _operation_completed->Acquire();
-    if (_operation_failed)
-    {
-        throw std::runtime_error{"擦除流程结束，出错了"};
-    }
-
-    SCB_CleanInvalidateDCache();
-}
-
-void bsp::Flash::EraseSector_NoIT(int32_t bank_index, int32_t sector_index)
-{
-    FLASH_EraseInitTypeDef def;
-
-    // 擦除类型为扇区擦除
-    def.TypeErase = FLASH_TYPEERASE_SECTORS;
-
-    // 选择 bank
-    switch (bank_index)
-    {
-    case 0:
-        {
-            def.Banks = FLASH_BANK_1;
-            break;
-        }
-    case 1:
-        {
-            def.Banks = FLASH_BANK_2;
-            break;
-        }
-    default:
-        {
-            throw std::out_of_range{"bank_index 超出范围"};
-        }
-    }
-
+    def.Banks = FLASH_BANK_2;
     def.Sector = SectorIndexToDefine(sector_index);
     def.NbSectors = 1;
 
@@ -303,17 +244,7 @@ void bsp::Flash::EraseSector_NoIT(int32_t bank_index, int32_t sector_index)
     SCB_CleanInvalidateDCache();
 }
 
-#pragma endregion
-
-void bsp::Flash::Read(int32_t bank_index, size_t addr, uint8_t *buffer, int32_t count)
-{
-    uint8_t *absolute_address = reinterpret_cast<uint8_t *>(GetAbsoluteAddress(bank_index, addr));
-    std::copy(absolute_address, absolute_address + count, buffer);
-}
-
-#pragma region 编程
-
-void bsp::Flash::Program(int32_t bank_index, size_t addr, uint8_t const *buffer)
+void bsp::Flash::Program(size_t addr, uint8_t const *buffer)
 {
     if (addr % MinProgrammingUnit() != 0)
     {
@@ -326,7 +257,7 @@ void bsp::Flash::Program(int32_t bank_index, size_t addr, uint8_t const *buffer)
     }
 
     HAL_StatusTypeDef result = HAL_FLASH_Program_IT(FLASH_TYPEPROGRAM_FLASHWORD,
-                                                    static_cast<uint32_t>(GetAbsoluteAddress(bank_index, addr)),
+                                                    static_cast<uint32_t>(BaseAddress() + addr),
                                                     reinterpret_cast<uint32_t>(buffer));
 
     if (result != HAL_StatusTypeDef::HAL_OK)
@@ -343,7 +274,11 @@ void bsp::Flash::Program(int32_t bank_index, size_t addr, uint8_t const *buffer)
     SCB_CleanInvalidateDCache();
 }
 
-void bsp::Flash::Program_NoIT(int32_t bank_index, size_t addr, uint8_t const *buffer)
+#pragma endregion
+
+#pragma region 编程
+
+void bsp::Flash::Program_NoIT(size_t addr, uint8_t const *buffer)
 {
     if (addr % MinProgrammingUnit() != 0)
     {
@@ -356,7 +291,7 @@ void bsp::Flash::Program_NoIT(int32_t bank_index, size_t addr, uint8_t const *bu
     }
 
     HAL_StatusTypeDef result = HAL_FLASH_Program(FLASH_TYPEPROGRAM_FLASHWORD,
-                                                 static_cast<uint32_t>(GetAbsoluteAddress(bank_index, addr)),
+                                                 static_cast<uint32_t>(BaseAddress() + addr),
                                                  reinterpret_cast<uint32_t>(buffer));
 
     if (result != HAL_StatusTypeDef::HAL_OK)
@@ -365,6 +300,12 @@ void bsp::Flash::Program_NoIT(int32_t bank_index, size_t addr, uint8_t const *bu
     }
 
     SCB_CleanInvalidateDCache();
+}
+
+void bsp::Flash::Read(size_t addr, uint8_t *buffer, int32_t count)
+{
+    uint8_t *absolute_address = reinterpret_cast<uint8_t *>(BaseAddress() + addr);
+    std::copy(absolute_address, absolute_address + count, buffer);
 }
 
 #pragma endregion
--- private_src/Flash.h
+++ private_src/Flash.h
@@ -34,96 +34,56 @@ namespace bsp
 
         /// @brief flash 的名称。
         /// @return
-        std::string Name() override
-        {
-            // 片内 flash
-            return "internal-flash";
-        }
+        std::string Name() override;
 
         void Lock() override;
         void Unlock() override;
 
 #pragma region flash 参数
-
-        /// @brief 获取此 flash 的 bank 数量。
-        /// @return
-        int32_t BankCount() const override
-        {
-            return 2;
-        }
-
         /// @brief 一个扇区的大小。单位：字节。
         /// @return
-        size_t SectorSize() const override
-        {
-            return static_cast<size_t>(128) << 10;
-        }
+        size_t SectorSize() const override;
 
-        /// @brief 获取指定 bank 的扇区数量。
-        /// @param bank_index
+        /// @brief 扇区数量。
         /// @return
-        int32_t GetBankSectorCount(int32_t bank_index) const override
-        {
-            return 8;
-        }
+        int32_t SectorCount() const override;
 
-        /// @brief 获取指定 bank 的基地址。
-        /// @param bank_index
+        /// @brief 本 flash 映射到内存中的区域的基地址。
         /// @return
-        size_t GetBankBaseAddress(int32_t bank_index) const override;
+        size_t BaseAddress() const override;
 
         /// @brief flash 的最小编程单位。单位：字节。
         /// @note 最小单位是一次编程必须写入这么多字节，即使要写入的数据没有这么多，在一次
         /// 写入后，整个单位大小的区域都无法再次写入了，除非擦除整个扇区。
         /// @return 返回此 flash 编程的最小单位。
-        int32_t MinProgrammingUnit() const override
-        {
-            return 32;
-        }
-
+        int32_t MinProgrammingUnit() const override;
 #pragma endregion
 
 #pragma region 擦除
-        /// @brief 擦除一整个 bank。
-        /// @note stm32h743 有 2 个 bank。典型用法是：bank1 用来存放程序，bank2 用来存放数据。
-        /// @warning bank_index 为 0 表示 bank1，索引为 1 表示 bank2。
-        /// @param bank_index
-        void EraseBank(int32_t bank_index) override;
+        /// @brief 擦除整个 flash.
+        void Erase() override;
 
-        /// @brief 擦除一整个 bank。
-        /// @note 本函数的实现方式是轮询标志位，不会使用中断和信号量。
-        /// @note stm32h743 有 2 个 bank。典型用法是：bank1 用来存放程序，bank2 用来存放数据。
-        /// @warning bank_index 为 0 表示 bank1，索引为 1 表示 bank2。
-        ///
-        /// @param bank_index
-        void EraseBank_NoIT(int32_t bank_index);
-
-        /// @brief 擦除指定 bank 的指定扇区。
-        /// @param bank_index
+        /// @brief 擦除指定扇区。
         /// @param sector_index 要擦除的扇区索引。
-        void EraseSector(int32_t bank_index, int32_t sector_index);
+        void EraseSector(int32_t sector_index) override;
         using bsp::IFlash::EraseSector;
 
+        /// @brief 擦除一整个 bank。
+        /// @note 本函数的实现方式是轮询标志位，不会使用中断和信号量。
+        /// @note stm32h743 有 2 个 bank。典型用法是：bank1 用来存放程序，bank2 用来存放数据。
+        /// @warning bank_index 为 0 表示 bank1，索引为 1 表示 bank2。
+        void EraseBank_NoIT();
+
         /// @brief 擦除指定 bank 的指定扇区。
         /// @note 本函数的实现方式是轮询标志位，不会使用中断和信号量。
         ///
-        /// @param bank_index
         /// @param sector_index 要擦除的扇区索引。
-        void EraseSector_NoIT(int32_t bank_index, int32_t sector_index);
+        void EraseSector_NoIT(int32_t sector_index);
 #pragma endregion
-
-        /// @brief 从 flash 中读取数据，写入 buffer。
-        /// @param bank_index
-        /// @param addr flash 中的数据相对于此 bank 的地址。
-        /// @param buffer 要将 flash 中的数据写入此缓冲区。
-        /// @param count 要读取多少个字节的数据。
-        void Read(int32_t bank_index, size_t addr, uint8_t *buffer, int32_t count) override;
 
 #pragma region 编程
         /// @brief 编程
         ///
-        /// @param bank_index
-        ///
         /// @param addr 要写入的数据相对于此 bank 的起始地址的地址。
         /// @note 此地址必须能被 MinProgrammingUnit 整除。
         ///
@@ -136,13 +96,11 @@ namespace bsp
         /// 不能整除抛出异常。
         /// @note 对于 4 字节对齐的情况，调用者可以创建 uint32_t 数组，然后
         /// 将 uint32_t const * 强制转换为 uint8_t const * 作为 buffer 传进来。
-        void Program(int32_t bank_index, size_t addr, uint8_t const *buffer) override;
+        void Program(size_t addr, uint8_t const *buffer) override;
 
         /// @brief 编程
         /// @note 本函数的实现方式是轮询标志位，不会使用中断和信号量。
         ///
-        /// @param bank_index
-        ///
         /// @param addr 要写入的数据相对于此 bank 的起始地址的地址。
         /// @note 此地址必须能被 MinProgrammingUnit 整除。
         ///
@@ -155,7 +113,13 @@ namespace bsp
         /// 不能整除抛出异常。
         /// @note 对于 4 字节对齐的情况，调用者可以创建 uint32_t 数组，然后
         /// 将 uint32_t const * 强制转换为 uint8_t const * 作为 buffer 传进来。
-        void Program_NoIT(int32_t bank_index, size_t addr, uint8_t const *buffer);
+        void Program_NoIT(size_t addr, uint8_t const *buffer);
 #pragma endregion
+
+        /// @brief 将 flash 的数据读取到缓冲区中
+        /// @param addr 相对于此 bank 的起始地址的地址。
+        /// @param buffer 读出的数据放到此缓冲区。
+        /// @param count 要读取的字节数。
+        void Read(size_t addr, uint8_t *buffer, int32_t count) override;
     };
 } // namespace bsp
