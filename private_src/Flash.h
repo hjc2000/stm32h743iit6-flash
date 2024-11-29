d85630479f9f60c27b6a739469f73ecc3c65b92e
hjc2000 <hjc.666@foxmail.com>
2024-11-29 11:12:07 +0800

    注释

--- private_src/Flash.h
+++ private_src/Flash.h
@@ -41,6 +41,7 @@ namespace bsp
         std::string Name() override;
 
         /// @brief 一个扇区的大小。单位：字节。
+        /// @note 擦除必须以扇区为单位，至少擦除一个扇区。
         /// @return
         size_t SectorSize() const override;
 
@@ -84,13 +85,13 @@ namespace bsp
 #pragma region 编程
         /// @brief 编程
         ///
-        /// @param addr 要写入的数据相对于此 bank 的起始地址的地址。
-        /// @note 此地址必须能被 MinProgrammingUnit 整除。
+        /// @param addr 要写入的数据在此 flash 中的地址。
+        /// @note 此地址必须能被 ProgrammingSize 整除。
         ///
-        /// @param buffer 要写入到 flash 的数据所在的缓冲区。大小为 MinProgrammingUnit。
-        /// @note 底层在编程时，会读取并且只会读取 MinProgrammingUnit 个字节。
-        /// @warning buffer 的字节数必须 >= MinProgrammingUnit ，否则因为底层无论如何都会读取
-        /// MinProgrammingUnit 个字节，所以将发生内存访问越界。
+        /// @param buffer 要写入到 flash 的数据所在的缓冲区。大小为 ProgrammingSize。
+        /// @note 底层在编程时，会读取并且只会读取 ProgrammingSize 个字节。
+        /// @warning buffer 的字节数必须 >= ProgrammingSize ，否则因为底层无论如何都会读取
+        /// ProgrammingSize 个字节，将会发生内存访问越界。
         /// @note 不同平台对 buffer 有对齐要求。例如 stm32 的 HAL 要求 buffer 要 4 字节
         /// 对齐。这里使用 uint8_t const * ，接口的实现者自己计算 buffer 能否被对齐字节数整除，
         /// 不能整除抛出异常。
@@ -117,9 +118,12 @@ namespace bsp
 #pragma endregion
 
         /// @brief 将 flash 的数据读取到缓冲区中
-        /// @param addr 相对于此 bank 的起始地址的地址。
+        /// @param addr 在此 flash 中的地址。
         /// @param buffer 读出的数据放到此缓冲区。
         /// @param count 要读取的字节数。
+        ///
+        /// @note 实现者需要将本函数实现为支持单字节读取。如果硬件不支持，可以自己准备一个足够大的数组，
+        /// 读取完后再将小于读取单位的字节数拷贝到 buffer 中。
         void Read(size_t addr, uint8_t *buffer, int32_t count) override;
     };
 } // namespace bsp
